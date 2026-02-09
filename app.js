8f6f983374e523681d7c324838ea87e26b80aecf
merchanjair1 <merchanjair1@gmail.com>
2026-02-09 15:46:32 -0500

    Cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require('dotenv').config();
+var cors = require('cors');
 var router = require('./routes/router');
 
 var app = express();
